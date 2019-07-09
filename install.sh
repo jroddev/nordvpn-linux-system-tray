2dd74dcd24766cddef945daeec562178a4185727
Jroddev <none>
2019-07-09 22:27:35 +1000

    created systemd service

--- install.sh
+++ install.sh
@@ -4,8 +4,20 @@ echo "Generating make files with qmake"
 qmake
 echo "Build nordvpn-linux-system-tray application"
 make
+cp nordvpn-system-tray /usr/sbin/
+cp nordvpn-system-tray.service /usr/lib/systemd/system/
+
+echo "Create init file for service"
+SERVICE_CONF_DIR=/usr/lib/systemd/system/nordvpn-system-tray.service.d/
+mkdir -p $SERVICE_CONF_DIR
+SERVICE_INIT_FILE=$SERVICE_CONF_DIR/init.sh
+echo "export XAUTHORITY=$XAUTHORITY" > $SERVICE_INIT_FILE
+chmod 755 $SERVICE_INIT_FILE
 
 echo "copying icons to /usr/share/pixmaps"
 cp nordvpn-icon-connected.png /usr/share/pixmaps/
 cp nordvpn-icon-disconnected.png /usr/share/pixmaps/
 
+systemctl daemon-reload
+systemctl start nordvpn-system-tray.service
+systemctl enable nordvpn-system-tray.service
--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo "disabling service"
+systemctl stop nordvpn-system-tray.service
+systemctl disable nordvpn-system-tray.service
+systemctl daemon-reload
+
+echo "removing nordvpn-system-tray service"
+rm /usr/sbin/nordvpn-system-tray
+rm /usr/lib/systemd/system/nordvpn-system-tray.service
+rm -rf /usr/lib/systemd/system/nordvpn-system-tray.service.d
 
 echo "removing icons from /usr/share/pixmaps"
 rm /usr/share/pixmaps/nordvpn-icon-connected.png
